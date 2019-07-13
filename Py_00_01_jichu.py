efb8ac76e2f610b76ca83f8ce04492865709a665
HBYAN <ilovejnote@163.com>
2019-07-13 23:56:30 +0800

    Add Py_jichu's print \n ... in

---
+++ Py_00_01_jichu.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# print absolute value of an integer
+
+a = 100
+if a >= 0:
+	print(a)
+else:
+	print(-a)
+
+# print string of \n...
+# If there is '' in '' use \ to turn
+print('I\'m "OK" !')
+print('I\'m \"OK\" !')
+# If there is enter in use \n to turn
+print('I\'m Learning\nPython.')
+print('\\\n\\')
+print('\\\t\\')
+# use r' or r'' or r''' to undo the \ turn action
+print(r'\\\t\\')
+# If there have lots of enter line then use ''' to use no \n
+print('''line1
+line2
+line3''')
+
+# Add homework at end
+
+# -*- coding:utf-8 -*-
+print(r'''hello,\n
+world''')
