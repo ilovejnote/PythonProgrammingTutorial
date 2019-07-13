f83af75329bce3d81a445aaefa629941fdd417b0
HBYAN <ilovejnote@163.com>
2019-07-13 22:51:23 +0800

    Add home work at end.

--- Py_00_00_helloworld.py
+++ Py_00_00_helloworld.py
@@ -4,3 +4,8 @@ print('The quick brown fox', 'jumps over', 'the lazy dog')
 print('100 + 200 =', 100 + 200)
 name = input('Please enter your name: ')
 print('Hello,', name)
+
+# After class && home work
+
+# -*- coding: utf-8 -*-
+print('1024 * 768 =', 1024 * 768)
