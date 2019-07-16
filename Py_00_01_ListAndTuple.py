29ad0b1f3efa538225d11a81f3ff54b7052afabf
HBYAN <ilovejnote@163.com>
2019-07-16 18:03:14 +0800

    Add ListAndTuple.py

---
+++ Py_00_01_ListAndTuple.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# init a list variable and print it out
+classmates = ['Michael', 'Bob', 'Tracy']
+print(classmates)
+# use len() can counts the number of the list .
+print(len(classmates))
+# How to index the number of the list
+i = 0
+while i<= (len(classmates ) - 1):
+    print(classmates[i])
+    i = i + 1
+#another method of index list
+print(classmates[0], classmates[1], classmates[2])
+print(classmates[-1], classmates[-2], classmates[-3])
