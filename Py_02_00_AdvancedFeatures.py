c16d64623054f498461eedd2c6a96ce99a1f659a
HBYAN <ilovejnote@163.com>
2019-07-25 11:35:38 +0800

    Add Python's Advanced Features.

---
+++ Py_02_00_AdvancedFeatures.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Advanced features for python
+# If you master python's data type; statement; function; In basic you can write lots of usefull programmings.
+# In class example :
+L= []
+n = 1
+while n <= 99:
+    print(n)
+    L.append(n)
+    n = n + 2
+# this is a 1,3,5,....99. List.
+# But in python the code is the less the better. So remember that there is Advanced Features for you.
+
